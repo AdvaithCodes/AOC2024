d23a203a7ad41f6de79ae705da7b4b4dbe85f5e0
AdvaithCodes <advaithnair0518@gmail.com>
2024-12-16 20:49:59 -0700

    Day 1 Complete!

--- AOC_Day_1.py
+++ AOC_Day_1.py
@@ -1,3 +1,16 @@
 #https://adventofcode.com/2024/day/1
 
-#hello there
+
+list_1 = [3,4,2,1,3,3]
+list_2 = [4,3,5,3,9,3]
+
+list_1.sort()
+list_2.sort()
+
+distance = 0
+
+for n1, n2 in zip(list_1, list_2):
+    distance = distance + abs(n1 - n2)
+    
+print(distance)
+    
